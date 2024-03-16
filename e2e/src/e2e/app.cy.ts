2259da3a61ea6570e5dcace18385a3d440e5359c
Alfredo Perez <alfredo.perez.q@gmail.com>
2024-03-16 09:15:23 -0500

    fix: lint

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,5 +1,6 @@
 describe('e2e', () => {
   beforeEach(() => cy.visit('/'));
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   it.skip('should display welcome message', () => {});
 });
