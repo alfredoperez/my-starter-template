97efca55adc13fd74636c88491a1add5919196fe
Alfredo Perez <alfredo.perez.q@gmail.com>
2024-03-16 09:23:37 -0500

    fix: e2e test

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,6 +1,7 @@
 describe('e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  it.skip('should display welcome message', () => {});
+  it.skip('should display welcome message', () => {
+    cy.get('h1').contains(/Users/);
+  });
 });
